6a13f3e81c281f428e9f5e569fe7b9ff24771aa1
sudha123 <sudhanaukala1997@gmail.com>
2022-11-08 07:17:38 +0530

    Fibonacci Series

---
+++ src/com/bridgelabz/logicalPrograms/FibonacciSeries.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.logicalPrograms;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        int num1 = 0;
+        int num2 = 1;
+
+        System.out.println(num1 + "\n" + num2);
+        for (int i = 0; i < 10; i++) {
+            int num3 = num1 + num2;
+            System.out.println(num3);
+            num1 = num2;
+            num2 = num3;
+        }
+    }
+}
