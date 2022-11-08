51d5e442b69e51130ace95515d4f9df3e9cf2441
sudha123 <sudhanaukala1997@gmail.com>
2022-11-08 08:25:29 +0530

    CouponNumber

---
+++ src/com/bridgelabz/logicalPrograms/CouponNumber.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.logicalPrograms;
+
+import java.util.Random;
+
+public class CouponNumber {
+    public static void main(String[] args) {
+        int winnerCouponNum=123456;
+        int couponNumber=0;
+        int couponCount=0;
+
+        while (couponNumber !=winnerCouponNum){
+            couponNumber=generateCoupon();
+            couponCount++;
+            System.out.println(couponNumber);
+        }
+        System.out.println("winner coupon : "+couponNumber);
+        System.out.println("No of coupons to match winner : "+couponCount);
+    }
+    static int generateCoupon(){
+        int num=0;
+        Random random=new Random();
+        while (num<100000){
+            num=random.nextInt(999999);
+        }
+        return num;
+    }
+}
